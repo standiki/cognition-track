c88940441dc046bfc2a6a5b9ce0403743729b7ec
Stanley Diki <stanleydiki@gmail.com>
2023-08-06 23:56:30 +0100

    Implementing Threads list or catalogue

--- thread-app/src/components/Threads.jsx
+++ thread-app/src/components/Threads.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function ThreadsList() {
+function ThreadsList({ threads }) {
   return (
-    <div>
-      <h2>Posted threads will show here</h2>
+    <div className="threads">
+      {/* <h2>Posted threads will show here</h2> */}
+      {threads.map((thread) => {
+        const { id, title, content } = thread;
+        return (
+          <article key={id} className="thread">
+            <h3>{title}</h3>
+            <p>{content}</p>
+          </article>
+        );
+      })}
     </div>
   );
 }

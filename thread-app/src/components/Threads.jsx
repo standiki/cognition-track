3697cb909c2d8366324e41b4cde6f4e027ff653e
Stanley Diki <stanleydiki@gmail.com>
2023-08-18 22:07:57 +0100

    Adding App Features Refactored some class names and the css styles Adding a button to show or hide long thread content

--- thread-app/src/components/Threads.jsx
+++ thread-app/src/components/Threads.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 function ThreadsList({ threads }) {
+  const [readMore, setReadMore] = useState(false);
+
   return (
-    <div className="threads">
+    <div className="threads-container">
       {/* <h2>Posted threads will show here</h2> */}
       {threads.map((thread) => {
         const { id, title, content } = thread;
         return (
           <article key={id} className="thread">
-            <h3>{title}</h3>
-            <p>{content}</p>
+            <div className="title">
+              <h3>{title}</h3>
+              <div className="thread-btn">
+                <FaTrash />
+                <FaEdit />
+              </div>
+            </div>
+            <p>
+              {readMore ? content : content.substring(0, 200)}
+              <button onClick={() => setReadMore(!readMore)}>
+                {readMore ? "show less" : "read more"}
+              </button>
+            </p>
           </article>
         );
       })}
